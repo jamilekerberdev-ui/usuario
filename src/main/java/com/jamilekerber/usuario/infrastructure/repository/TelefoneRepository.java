7b873cdecddc1c24333586a2a5565da44a5bfeb8
Jamile Kerber <jamilekerber.dev@gmail.com>
2026-05-06 10:43:41 -0300

    Revert "Develop"

--- src/main/java/com/jamilekerber/usuario/infrastructure/repository/TelefoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jamilekerber.usuario.infrastructure.repository;
-
-import com.jamilekerber.usuario.infrastructure.entity.Telefone;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TelefoneRepository extends JpaRepository<Telefone, Long>{
-}
