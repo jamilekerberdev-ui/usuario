9c3b1f5616eb9b79158ea1311f88e3784ba9a86a
Jamile Kerber <jamilekerber.dev@gmail.com>
2026-05-06 16:45:36 -0300

    Adicionado método para salvar dados do usuário

---
+++ src/main/java/com/jamilekerber/usuario/business/dto/TelefoneDTO.java
@@ -0,0 +1,14 @@
+package com.jamilekerber.usuario.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TelefoneDTO {
+
+    private String numero;
+    private String ddd;
+}
